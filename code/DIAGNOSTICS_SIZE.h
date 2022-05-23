1a7b08e4dd68ac3be1c5a611d375d000ed456825
Jody Klymak <jklymak@gmail.com>
2022-05-23 06:12:22 -0700

    gendata for Bute12: Higher near-surface stratification; bigger domain to prevent seiching.  More horizontal diffusion; observed TS to 230 m; Qnet=500, uw=15 m/s, Non hydrostatic!!, KL10 off, fix spongewieght,  turn on diff for salt! and set advection scheme to default (2); implicit free surf versus rigid lid; closer to convection example, Kh = 4e-3, O2 no airsea flux

--- code/DIAGNOSTICS_SIZE.h
+++ code/DIAGNOSTICS_SIZE.h
@@ -22,9 +22,9 @@ C  and "diagSt_size" (statistics-diags) since values here are deliberately small
       INTEGER    diagSt_size
       PARAMETER( ndiagMax = 500 )
       PARAMETER( numlists = 12, numperlist = 50, numLevels=2*Nr )
-      PARAMETER( numDiags = 16*Nr )
+      PARAMETER( numDiags = 18*Nr )
       PARAMETER( nRegions = 0 , sizRegMsk = 1 , nStats = 4 )
-      PARAMETER( diagSt_size = 12*Nr )
+      PARAMETER( diagSt_size = 14*Nr )
 
 
 CEH3 ;;; Local Variables: ***
