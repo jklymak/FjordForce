6990c57e241c80bdec429158764712e3b21e6c00
jklymak <jklymak@gmail.com>
2022-05-25 10:11:36 -0400

    gendata for Bute14: Higher res, observed TS to 230 m; Qnet=500, uw=15 m/s, Non hydrostatic!!, KL10 off, Kh = 4e-4, O2 with airsea flux;

--- input/gendata.py
+++ input/gendata.py
@@ -189,7 +189,7 @@ fig.savefig(outdir+'/figs/topo.png')
 # dz:
 # dz is from the surface down (right?).  Its saved as positive.
 dz = np.ones(nz)
-for i in range(120, nz):
+for i in range(115, nz):
     dz[i] = dz[i-1] * 1.03
 
 with open(indir+"/delZ.bin", "wb") as f:
