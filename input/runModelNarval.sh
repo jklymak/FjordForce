0ff174e20e488c441fc5d3052db1d456197463a5
jklymak <jklymak@gmail.com>
2022-05-26 03:45:38 -0400

    gendata for Bute14: Higher res, observed TS to 230 m; Qnet=500, uw=15 m/s, Non hydrostatic!!, KL10 off, Kh = 4e-4, O2 with airsea flux;

---
+++ input/runModelNarval.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH --account=def-jklymak
+#SBATCH --mail-user=jklymak@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --time=0-00:10
+#SBATCH --mem=0
+
+# sbatch --job-name={todo} runModelNarval.sh
+# run from runAll.sh  start and stop come from -v arguments.
+
+
+# module swap mpt compiler/intelmpi
+
+# module load netcdf-fortran-mpi/4.5.2
+# module load python/3.9.6
+
+
+
+cd ${SLURM_SUBMIT_DIR}
+
+printf "workdir? ${SLURM_SUBMIT_DIR}\n"
+pwd
+
+top=${SLURM_JOB_NAME}
+printf "top: $top\n"
+results=${SLURM_SUBMIT_DIR}/../results/
+outdir=$results$top
+
+cd $outdir/input
+pwd
+ls -al ../build/mitgcmuv
+
+# python moddata.py --startTime=$start --endTime=$stop --deltaT=$dt
+
+printf "Starting: $outdir\n"
+module list
+which srun
+srun ../build/mitgcmuv > mit.out
