32f904977b1ef83cadc9e43ec09f1f42fc026823
Jody Klymak <jklymak@gmail.com>
2022-11-28 16:32:14 -0800

    gendata for Bute3d36: Symmetric, bigger receiving basin with roughness in it.  Tau=0.1 N/m^2 (10 m/s) versus 0.225 N/m^2. Lat = 45; f=1.000e-04 Constant Nsq0=0.000688. No wind startup = just turn it on. Remove the roughness wavey = False. I don't understand why there is still assymetry even if f=0... Not as aggressive a grid growth. Leith off? advschemes = 77 for both salt and temp No sponge.  Step temperature, and make temperature zero in inlet and 10 everywhere else. This will allow us to determine the next exchange of tracer between the basin and inlet.

--- input/gendata.py
+++ input/gendata.py
@@ -27,10 +27,10 @@ wavey = False
 Nsq0 = 3.44e-4
 tAlpha = 0.0e-4
 sBeta = 7.4e-4
-Nsqfac = 1.0
+Nsqfac = 2.0
 Nsq0 = Nsq0 * Nsqfac
 
-runname='Bute3d35'
+runname='Bute3d36'
 comments = f"""
 Symmetric, bigger receiving
 basin with roughness in it.  Tau={wind**2*1e-3} N/m^2 ({wind} m/s) versus 0.225 N/m^2.
