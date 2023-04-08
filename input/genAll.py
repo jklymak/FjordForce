1e37764e7b71feb0acb4946bb1535314f4a84632
Jody Klymak <jklymak@gmail.com>
2023-04-08 12:32:57 -0700

    gendata for Bute3d52:   As run 43.  Tau=0.9 N/m^2 (30 m/s)   Lat = 45; f=1.000e-04   Constant Nsq; steady forcing....   advschemes = 77 for both salt and temp   Constant Nsq, Temperature passive, but set to something that will   have reasonable flux.  Can't vary in space!  8.9 degrees C about right.

---
+++ input/genAll.py
@@ -0,0 +1,16 @@
+import gendata
+
+num = 1
+for wind in [30, 20, 15, 10]:
+    for NsqFac in [0.5, 1, 2]:
+        if ((NsqFac == 1) and (wind == 20)):
+            print(f'Already Created Nsq={NsqFac}, wind={wind}')
+            # this is run50
+            continue
+        num += 1
+        runnum = 50 + num
+        with open('./genall_runs.txt', 'a') as fout:
+            fout.write(f'Bute3d{runnum}: wind={wind} NsqFac={NsqFac}')
+        gendata.gendata(runnumber=runnum, wind=wind, NsqFac=NsqFac)
+
+
