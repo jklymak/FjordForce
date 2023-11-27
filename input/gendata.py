124698e84f8ef0f7f38972d1377c16e00530178e
Jody Klymak <jklymak@gmail.com>
2023-11-27 10:27:00 -0800

    gendata for Fjord4: Lower res (dx=100, nz=20) Atmopsheric Load; RBCS Compiled forcing

--- input/gendata.py
+++ input/gendata.py
@@ -343,7 +343,7 @@ with open(indir+'Tforce.bin', 'wb') as f:
     weight.tofile(f)
 # seasurface height forcing.  tidal!
 
-amp = 0   # m
+amp = 100  # m
 om = np.pi * 2 / 12.4 / 3600 # rad/s
 
 weight = np.zeros((nt, ny, nx)) + (amp*np.cos(om * t))[:, np.newaxis, np.newaxis]
